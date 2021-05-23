f8546586dafb0548c054ee5cd69cbab8afac1ba6
Sandeep Dass <sandeepdass003@gmail.com>
2021-05-23 22:15:17 +0530

    Initial Project Checkin

---
+++ resultmanagement/src/test/java/com/pgdca/resultmanagement/ResultManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgdca.resultmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
