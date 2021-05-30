2f630a19e18bce4e23249ae56da6f59fe704e25d
Sandeep Dass <sandeepdass003@gmail.com>
2021-05-30 21:44:45 +0530

    Adding City_Info.sql file

--- resultmanagement/src/main/java/com/pgdca/resultmanagement/jdbc/entity/CityInfo.java
+++ resultmanagement/src/main/java/com/pgdca/resultmanagement/jdbc/entity/CityInfo.java
@@ -13,7 +13,7 @@ public class CityInfo {
 	@Column(name = "id", nullable = false, length = 10)
 	private String id;
 	
-	@Column(name = "name", nullable = false, length = 30)
+	@Column(name = "name", nullable = false, length = 50)
 	private String name;
 	
 	public CityInfo() {
