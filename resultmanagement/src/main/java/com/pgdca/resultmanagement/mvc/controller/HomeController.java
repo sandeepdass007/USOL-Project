1a7dd2b10995a910e9ea95a9dfac5dd12c016744
Sandeep Dass <sandeepdass003@gmail.com>
2021-06-01 19:56:39 +0530

    If student is already login then he/she cannot login again

--- resultmanagement/src/main/java/com/pgdca/resultmanagement/mvc/controller/HomeController.java
+++ resultmanagement/src/main/java/com/pgdca/resultmanagement/mvc/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.pgdca.resultmanagement.mvc.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 class HomeController {
 
 	@GetMapping({"/", "/welcome"})
-	public String showHomePage() {
+	public String showHomePage(HttpSession httpSession) {
+		String username = (String) httpSession.getAttribute("username");
+		if(StringUtils.hasText(username)) {
+			return "redirect:/student/";
+		}
 		return "home-page";
 	}
 }
