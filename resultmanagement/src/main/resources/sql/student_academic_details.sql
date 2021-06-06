7bd292b7536dfaf9d0aaaca99021db68607f8da7
MEHAR KAUR THIND <meharkthind@gmail.com>
2021-06-06 21:08:41 +0530

    Uploaded table student_academic_details in the sql folder

---
+++ resultmanagement/src/main/resources/sql/student_academic_details.sql
@@ -0,0 +1,9 @@
+-- select * from student_academic_detail;	
+desc student_academic_detail;
+insert into student_academic_detail values('801','1010','MScMathHS','Res3',1,'2019-01-01');
+insert into student_academic_detail values('802','1011','BECSE','Res3',1,'2013-01-01');
+insert into student_academic_detail values('803','1012','BAEngHS','Res3',1,'2018-01-01');
+insert into student_academic_detail values('804','1010','MScMathHS','Res1',1,'2019-07-01');
+insert into student_academic_detail values('805','1011','BECSE','Res3',1,'2013-07-01');
+insert into student_academic_detail values('806','1012','BAEngHS','Res1',1,'2018-07-01');
+
