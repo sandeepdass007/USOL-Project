9370515846f3cd11f21f68ed8941ba7061aa30da
lenovo <lenovo@DESKTOP-4FJTBIH>
2021-06-08 12:09:55 -0700

    Updated 2 tables for minor errors and added step 2 data entry for 4 tables namely master_marks, reappear_info, result_info, student_academic_details

--- resultmanagement/src/main/resources/sql/student_academic_details.sql
+++ resultmanagement/src/main/resources/sql/student_academic_details.sql
@@ -1,9 +1,14 @@
 -- select * from student_academic_detail;	
 desc student_academic_detail;
-insert into student_academic_detail values('801','1010','MScMathHS','Res3',1,'2019-01-01');
-insert into student_academic_detail values('802','1011','BECSE','Res3',1,'2013-01-01');
-insert into student_academic_detail values('803','1012','BAEngHS','Res3',1,'2018-01-01');
-insert into student_academic_detail values('804','1010','MScMathHS','Res1',1,'2019-07-01');
-insert into student_academic_detail values('805','1011','BECSE','Res3',1,'2013-07-01');
-insert into student_academic_detail values('806','1012','BAEngHS','Res1',1,'2018-07-01');
+insert into student_academic_detail values('801','MScMathHS','1010',1,'2019-01-01','Res3');
+insert into student_academic_detail values('802','BECSE','1011',1,'2013-01-01','Res3');
+insert into student_academic_detail values('803','BAEngHS','1012',1,'2018-01-01','Res3');
+insert into student_academic_detail values('804','MScMathHS','1010',1,'2019-07-01','Res1');
+insert into student_academic_detail values('805','BECSE','1011',1,'2013-07-01','Res3');
+insert into student_academic_detail values('806','BAEngHS','1012',1,'2018-07-01','Res1');
 
+
+-- step 2
+
+insert into student_academic_detail values('807','BECSE','1011',1,'2014-01-01','Res1');
+insert into student_academic_detail values('808','BECSE','1011',2,'2013-01-07','Res1');
