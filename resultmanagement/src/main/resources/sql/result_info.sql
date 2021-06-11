36fcfb70422da93921c45ad696467e400c8f20b1
lenovo <lenovo@DESKTOP-4FJTBIH>
2021-06-11 10:59:56 +0530

    Updated CSE Sem 3-8 result details in result_info sql file

--- resultmanagement/src/main/resources/sql/result_info.sql
+++ resultmanagement/src/main/resources/sql/result_info.sql
@@ -1,4 +1,4 @@
--- select * from result_info;
+select * from result_info;
 Insert into result_info values('601','1010','2019-01-01','Res1','MatDis1','Mat1Sub1');
 Insert into result_info values('602','1010','2019-01-01','Res1','MatDis2','Mat1Sub2');
 Insert into result_info values('603','1010','2019-01-01','Res1','MatDis3','Mat1Sub3');
@@ -29,7 +29,38 @@ Insert into result_info values('623','1011','2013-07-01','Res1','CSEDis9','CSE2S
 Insert into result_info values('624','1011','2013-07-01','Res1','CSEDis10','CSE2Sub5');
 Insert into result_info values('625','1011','2014-01-01','Res1','CSEDis5','CSE1Sub5');
 
--- Step 3 (Entering subject level results for PGDCA)
+-- Step 3 (Entering subject level results for CSE Sem 3-8 and PGDCA)
+Insert into result_info values('626','1011','2014-01-01','Res1','CSEDis11','CSE3Sub1');
+Insert into result_info values('627','1011','2014-01-01','Res1','CSEDis12','CSE3Sub2');
+Insert into result_info values('628','1011','2014-01-01','Res1','CSEDis13','CSE3Sub3');
+Insert into result_info values('629','1011','2014-01-01','Res1','CSEDis14','CSE3Sub4');
+Insert into result_info values('630','1011','2014-01-01','Res1','CSEDis15','CSE3Sub5');
+Insert into result_info values('631','1011','2014-07-01','Res1','CSEDis16','CSE4Sub1');
+Insert into result_info values('632','1011','2014-07-01','Res1','CSEDis17','CSE4Sub2');
+Insert into result_info values('633','1011','2014-07-01','Res1','CSEDis18','CSE4Sub3');
+Insert into result_info values('634','1011','2014-07-01','Res1','CSEDis19','CSE4Sub4');
+Insert into result_info values('635','1011','2014-07-01','Res1','CSEDis20','CSE4Sub5');
+Insert into result_info values('636','1011','2015-01-01','Res1','CSEDis21','CSE5Sub1');
+Insert into result_info values('637','1011','2015-01-01','Res1','CSEDis22','CSE5Sub2');
+Insert into result_info values('638','1011','2015-01-01','Res1','CSEDis23','CSE5Sub3');
+Insert into result_info values('639','1011','2015-01-01','Res1','CSEDis24','CSE5Sub4');
+Insert into result_info values('640','1011','2015-01-01','Res1','CSEDis25','CSE5Sub5');
+Insert into result_info values('641','1011','2015-01-07','Res1','CSEDis26','CSE6Sub1');
+Insert into result_info values('642','1011','2015-01-07','Res1','CSEDis27','CSE6Sub2');
+Insert into result_info values('643','1011','2015-01-07','Res1','CSEDis28','CSE6Sub3');
+Insert into result_info values('644','1011','2015-01-07','Res1','CSEDis29','CSE6Sub4');
+Insert into result_info values('645','1011','2015-01-07','Res1','CSEDis30','CSE6Sub5');
+Insert into result_info values('646','1011','2016-01-01','Res1','CSEDis31','CSE7Sub1');
+Insert into result_info values('647','1011','2016-01-01','Res1','CSEDis32','CSE7Sub2');
+Insert into result_info values('648','1011','2016-01-01','Res1','CSEDis33','CSE7Sub3');
+Insert into result_info values('649','1011','2016-01-01','Res1','CSEDis34','CSE7Sub4');
+Insert into result_info values('650','1011','2016-01-01','Res1','CSEDis35','CSE7Sub5');
+Insert into result_info values('651','1011','2016-07-01','Res1','CSEDis36','CSE8Sub1');
+Insert into result_info values('652','1011','2016-07-01','Res1','CSEDis37','CSE8Sub2');
+Insert into result_info values('653','1011','2016-07-01','Res1','CSEDis38','CSE8Sub3');
+Insert into result_info values('654','1011','2016-07-01','Res1','CSEDis39','CSE8Sub4');
+Insert into result_info values('655','1011','2016-07-01','Res1','CSEDis40','CSE8Sub5');
+
 Insert into result_info values('656','1013','2021-01-01','Res1','PGDDis1','PGD1Sub1');
 Insert into result_info values('657','1013','2021-01-01','Res1','PGDDis2','PGD1Sub2');
 Insert into result_info values('658','1013','2021-01-01','Res1','PGDDis3','PGD1Sub3');
