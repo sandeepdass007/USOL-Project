0573db4d8f97969849ef4876efc7061400b06337
lenovo <lenovo@DESKTOP-4FJTBIH>
2021-06-13 03:59:20 +0530

    Updated tables course_info and student_detail for improvement in home page charts by adding dummy students relevant data

--- resultmanagement/src/main/resources/sql/course_info.sql
+++ resultmanagement/src/main/resources/sql/course_info.sql
@@ -5,5 +5,14 @@ insert into course_info values ('BECSE','Bachelor of Engineering (Computer Scien
 insert into course_info values ('BAEngHS', 'Bachelor of Arts (English Hons)', 'CrsTyp1', 6, 3);
 
 
+
 -- step 3 (New course being added)
 insert into course_info values ('PGDCA', 'Post Graduate Diploma in Computer Applications' , 'CrsTyp4', 2, 1);
+
+
+-- step 6 (Adding new courses for homepage visuals)
+insert into course_info values ('LLB', 'Bachelor of Legislative Law' , 'CrsTyp1', 6, 3);
+insert into course_info values ('MBBS' , 'Bachelor of Medicine and a Bachelor of Surgery', 'CrsTyp1', 10,5);
+insert into course_info values ('PhD(Urdu)', 'Doctor of Philosophy (Urdu)', 'CrsTyp3', 14, 7);
+insert into course_info values ('CC(French)', 'Certificate Course in French', 'CrsTyp5', 2,1);
+insert into course_info values('BCA', 'Bachelor of Computer Applications', 'CrsTyp6', 6, 3);
